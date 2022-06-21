bd02414646413b2c436ebc5c28c92d7537163766
gurwadhwa7 <simran.gur3538@gmail.com>
2022-06-21 19:39:01 +0530

    Video Ascii

--- videoToAscii.py
+++ videoToAscii.py
@@ -17,41 +17,26 @@ def get_data(mode):
   char_list = char_set[mode]
   return char_list, font, scale
 
-# def display_video(frame_list, fps):
-#   while (True):
-#     for i in frame_list:
-#       sys.stdout.write(i)
-#       time.sleep(1 / fps)
-
 def main():
   bg = 'black'
   if bg == 'white':
-    bg_code = 255
-  else:
-    bg_code = 0
+    bg_code = (255, 255, 255)
+  elif bg == 'black':
+    bg_code = (0, 0, 0)
   
   char_list, font, scale = get_data("complex")
   char_count = len(char_list)
   col = 120
                                                                  
   cam = cv.VideoCapture("./videos/input.mp4")
-  frames_count = 0
-  # frame_list = []
   fps = cam.get(cv.CAP_PROP_FPS)
-  # height = int(cam.get(cv.CAP_PROP_FRAME_HEIGHT))
-  # width = int(cam.get(cv.CAP_PROP_FRAME_WIDTH))
-  # video_writer = cv.VideoWriter('./videos/output.avi', cv.VideoWriter_fourcc('P','I','M','1'), fps, (width, height), isColor=False)
   if fps > 60:
     fps = 30
 
-  while (True):
+  for frame_idx in range(int(cam.get(cv.CAP_PROP_FRAME_COUNT))):
     ret, frame = cam.read()
-    if ret:
-      curr_frame = cv.cvtColor(cv.UMat(frame), cv.COLOR_BGR2GRAY)
-      frames_count += 1
-    else:
-      break
-    height, width = curr_frame.shape
+    curr_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    height, width, _ = curr_frame.shape
     base_w = width / col
     base_h = scale * base_w
     row = int(height / base_h)
@@ -66,29 +51,33 @@ def main():
     char_w, char_h = font.getsize("A")
     res_w = char_w * col
     res_h = scale * char_h * row
-    res1 = Image.new("L", (res_w, res_h), bg_code) 
+    res1 = Image.new("RGB", (res_w, res_h), bg_code) 
     draw = ImageDraw.Draw(res1)
 
     for i in range(row):
-      min_h = min(int((i+1)*base_h), height)
-      pixeli = int(i*base_h)
-      line = "".join([char_list[min(int(np.mean(curr_frame[pixeli:min_h, int(j*base_w):min(int((j+1)*base_w), width)]) / 255*char_count), char_count-1)] for j in range(col)]) + "\n"
-      draw.text((0, i*char_h), line, fill = 255 - bg_code, font = font)
+      for j in range(col):
+        min_h = min(int((i+1)*base_h), height)
+        min_w = min(int((j+1)*base_w), width)
+        pixeli = int(i*base_h)
+        pixelj = int(j*base_w)
+        partial_img = curr_frame[pixeli:min_h, pixelj:min_w, :]
+        partial_avg_color = np.sum(np.sum(partial_img, axis = 0), axis = 0) / (base_h * base_w)
+        partial_avg_color = tuple(partial_avg_color.astype(np.int32).tolist())
+        c = char_list[min(int(np.mean(partial_img)*char_count / 255), char_count - 1)]
+        draw.text((j*char_w, i*char_h), c, fill = partial_avg_color, font = font)
 
     if bg == "white":
       partial_img = ImageOps.invert(res1).getbbox()
-    else:
+    elif bg == "black":
       partial_img = res1.getbbox()
-
     res1 = res1.crop(partial_img)
-    cv.imwrite("Frame.jpg", res1)
-      # video_writer.write(res1)
-      # frame_list.append("/n".join(res1))
+    res1 = np.array(res1)
+    cv.imshow("Frame", res1)
+    if cv.waitKey(10) & 0xFF == ord('q'):
+      break
     
   cam.release()
   cv.destroyAllWindows()
-  # video_writer.release()
-  # display_video(frame_list, fps)
 
 if __name__ == '__main__':
   main()
--- videoToGrayAscii.py
+++ videoToGrayAscii.py
@@ -1,18 +1,10 @@
-import imp
+import os
 import sys
 from time import time
 from tkinter import Frame
-from typing import overload
 import cv2 as cv
 import numpy as np
-import argparse
-# import time
-# import sys
-# import os
-# import json
-# import random
 from PIL import Image, ImageDraw, ImageOps, ImageFont
-# from pkgutil import get_data
 
 char_set = {
   "standard": "@%#*+=-:. ",
@@ -25,58 +17,26 @@ def get_data(mode):
   char_list = char_set[mode]
   return char_list, font, scale
 
-# def get_frames(frame):
-#   image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-#   out = Image.fromarray(image)
-#   return out
-
-# def take_input(cap, resolution):
-#   set_of_frames = []
-#   duration = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
-#   it = 0
-#   while (True):
-#     it += 1
-#     ret, frame = cap.read()
-#     if ret:
-#       curr_frame = get_frames(frame)
-#       resized = 
-
-
-def create_object():
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--input", type=str, default="videos/input.mp4", help="Input video")
-  parser.add_argument("--output", type=str, default="videos/output.mp4", help="Output video")
-  parser.add_argument("--col", type=int, default=100, help="Characters count in output width")
-  parser.add_argument("--fps", type=int, default=0, help="Number of frames per second")
-  parser.add_argument("--overlay_ratio", type=float, default=0.2, help="Overlay width ratio")
-  args = parser.parse_args()
-  return args
-
-def main(object):
+def main():
   bg = 'black'
   if bg == 'white':
     bg_code = 255
   else:
     bg_code = 0
-
+  
   char_list, font, scale = get_data("complex")
   char_count = len(char_list)
-  col = object.col
+  col = 120
+                                                                 
+  cam = cv.VideoCapture("./videos/input.mp4")
+  fps = cam.get(cv.CAP_PROP_FPS)
+  if fps > 60:
+    fps = 30
 
-  cap = cv.VideoCapture(object.input)
-  if object.fps == 0:
-    fps = int(cap.get(cv.CAP_PROP_FPS))
-  else:
-    fps = object.fps
-
-  while cap.isOpened():
-    flag, frame = cap.read()
-    if flag:
-      gray_img = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    else:
-      break
-
-    height, width = gray_img.shape
+  for frame_idx in range(int(cam.get(cv.CAP_PROP_FRAME_COUNT))):
+    ret, frame = cam.read()
+    curr_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    height, width = curr_frame.shape
     base_w = width / col
     base_h = scale * base_w
     row = int(height / base_h)
@@ -97,31 +57,22 @@ def main(object):
     for i in range(row):
       min_h = min(int((i+1)*base_h), height)
       pixeli = int(i*base_h)
-      line = "".join([char_list[min(int(np.mean(gray_img[pixeli:min_h, int(j*base_w):min(int((j+1)*base_w), width)]) / 255*char_count), char_count-1)] for j in range(col)]) + "\n"
+      line = "".join([char_list[min(int(np.mean(curr_frame[pixeli:min_h, int(j*base_w):min(int((j+1)*base_w), width)]) / 255*char_count), char_count-1)] for j in range(col)]) + "\n"
       draw.text((0, i*char_h), line, fill = 255 - bg_code, font = font)
 
     if bg == "white":
       partial_img = ImageOps.invert(res1).getbbox()
     else:
       partial_img = res1.getbbox()
-    
-    res1 = res1.crop(partial_img)
-    res1 = cv.cvtColor(np.array(res1), cv.COLOR_GRAY2BGR)
-    res1 = np.array(res1)
-    try:
-      out
-    except:
-      out = cv.VideoWriter(object.output, cv.VideoWriter_fourcc(*'MP4V4'), fps, ((res1.shape[1], res1.shape[0])))
-    
-    cap.release()
-    out.release()
 
-def display(set_of_frames, fps):
-  while (True):
-    for i in set_of_frames:
-      sys.stdout.write(i)
-      time.sleep(1 / fps)
+    res1 = res1.crop(partial_img)
+    res1 = np.array(res1)
+    cv.imshow("Frame", res1)
+    if cv.waitKey(10) & 0xFF == ord('q'):
+      break
+    
+  cam.release()
+  cv.destroyAllWindows()
 
 if __name__ == '__main__':
-  object = create_object()
-  main(object)
+  main()
